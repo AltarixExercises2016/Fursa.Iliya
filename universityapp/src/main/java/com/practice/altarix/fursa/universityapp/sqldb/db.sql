95009c48e7c4fa2864c6eee61adf4a73daa70d03
fursailya <mkb24821>
2016-12-26 22:06:18 +0400

    SQL script!!!

--- universityapp/src/main/java/com/practice/altarix/fursa/universityapp/sqldb/db.sql
+++ universityapp/src/main/java/com/practice/altarix/fursa/universityapp/sqldb/db.sql
@@ -9,8 +9,12 @@ CREATE TABLE `Lesson` (
 	`id`	INTEGER PRIMARY KEY AUTOINCREMENT,
 	`name`	TEXT,
 	`auditory`	INTEGER,
+	`lesson_day` TEXT
 	`type`	TEXT,
 	`teacher_id`	INTEGER,
 	 FOREIGN KEY(`teacher_id`) REFERENCES Teacher(id)
 );
-
+-- выбор всех данных по дню!!!
+--SELECT Lesson.name, Lesson.time, Lesson.day, Teacher.name
+--FROM Lesson, Teacher
+--WHERE Lesson.day = 'Пятница' AND Teacher.id = Lesson.teacher_id
